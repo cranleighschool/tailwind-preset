932121ca38da1ef83c2c0f2f015aff110b84563a
Fred Bradley <hello@fredbradley.co.uk>
2022-10-28 21:14:27 +0100

    Create cranleigh-tailwind-preset.js

---
+++ cranleigh-tailwind-preset.js
@@ -0,0 +1,31 @@
+module.exports = {
+  theme: {
+    colors: {
+      primary: {
+        senior: '#0c223f',
+        prep: '#8cb7e8',
+        },
+      darkblue: '#0c223f',
+      cransenior: '#0c223f',
+      cranprep: '#8cb7e8',
+      lightblue: '#8cb7e8',
+      maroon: '#ab162b',
+      yellow: '#ffc627',
+      darkgrey: '#545759',
+      lightgrey: '#8a8bbc',
+      sixthformgold: '#e2bd4b',
+      sixthformdarkgold: '#936824',
+      sixthformmaroon: '#8e1537',
+      cubitt: '#00864a',
+      east: 	'#8db3df',
+      south:  '#2bbbab',
+      west: 	'#DA1884',
+      loveday:'#ea2127',
+      north:	'#991a36',
+      rhodes: '#fe3200',
+      martlet:'#64369f'
+    },
+    extend: {
+    }
+  },
+}
